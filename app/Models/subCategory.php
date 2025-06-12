48de05aadc53202dc9bf63dffca22e83b98a5c9c
johmison.php.ph <johmison.php.ph@tripleonetech.net>
2025-06-12 13:13:03 +0800

    fix error

--- app/Models/subCategory.php
+++ app/Models/subCategory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use App\Models\ChildCategory;
 
 class subCategory extends Model
 {
@@ -26,6 +27,6 @@ class subCategory extends Model
 
     public function childCategories()
     {
-        return $this->hasMany(ChildCategory::class, 'sub_category_id', 'external_unique_id');
+        return $this->hasMany(childCategory::class, 'sub_category_id', 'external_unique_id');
     }
 }
