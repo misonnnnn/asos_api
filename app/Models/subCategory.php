7403d746dd623d8e1f823b1746eaf3e35fd527d7
johmison.php.ph <johmison.php.ph@tripleonetech.net>
2025-06-12 13:48:43 +0800

    Fix

--- app/Models/subCategory.php
+++ app/Models/subCategory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
-use App\Models\ChildCategory;
+use App\Models\childCategory;
 
 class subCategory extends Model
 {
